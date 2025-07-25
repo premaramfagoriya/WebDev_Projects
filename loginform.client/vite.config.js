c0f5f078e8a7965f03c29ff84ea3e42d673f26f9
premaramfagoriya <premaramfagoriya@gmail.com>
2025-07-25 12:24:20 +0530

    change vit

--- loginform.client/vite.config.js
+++ loginform.client/vite.config.js
@@ -2,30 +2,24 @@
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 
-const isDev = process.env.NODE_ENV !== 'production' && !process.env.VERCEL;
-const httpsOptions = isDev ? (await import('./https-config.js')).httpsOptions : false;
-
-const target = process.env.ASPNETCORE_HTTPS_PORT
-    ? `https://localhost:${process.env.ASPNETCORE_HTTPS_PORT}`
-    : process.env.ASPNETCORE_URLS
-        ? process.env.ASPNETCORE_URLS.split(';')[0]
-        : 'https://localhost:7136';
-
 export default defineConfig({
     plugins: [plugin()],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     },
     server: {
         proxy: {
             '^/weatherforecast': {
-                target,
-                secure: false,
-            },
+                target: 'https://localhost:7136',
+                secure: false
+            }
         },
-        port: parseInt(process.env.DEV_SERVER_PORT || '64033'),
-        https: httpsOptions,
-    },
+        port: 64033,
+        https: process.env.NODE_ENV === 'development' ? {
+            key: './certs/key.pem',
+            cert: './certs/cert.pem'
+        } : false
+    }
 });
